2ed1695d140f1db4a4b9bdf3fd46ff002c922816
Cicero Ribeiro <ciceromirandar@gmail.com>
2023-10-22 15:12:21 -0300

    Create speed gauge

--- src/galaxy.js
+++ src/galaxy.js
@@ -2,37 +2,39 @@
  * Represents a galaxy and its contained particles.
  */
 class Galaxy {
-    /**
-     * Creates a galaxy.
-     * @param {number} size - Number of particles to be created in galaxy
-     */
-    constructor(size) {
-        this.size = size;
-        this.particles = [];
+  /**
+   * Creates a galaxy.
+   * @param {number} size - Number of particles to be created in galaxy
+   * @param {Gauge} gauge - speed gauge
+   */
+  constructor(size, gauge) {
+    this.size = size;
+    this.gauge = gauge;
+    this.particles = [];
 
-        for (let i = 0; i < this.size; i++) {
-            const particle = new Particle();
-            this.particles.push(particle);
-        }
+    for (let i = 0; i < this.size; i++) {
+      const particle = new Particle(gauge);
+      this.particles.push(particle);
     }
+  }
 
-    /**
-     * Updates galaxy and its contained particles' state.
-     */
-    update() {
-        for (const particle of this.particles) {
-            particle.update();
-        }
+  /**
+   * Updates galaxy and its contained particles' state.
+   */
+  update() {
+    for (const particle of this.particles) {
+      particle.update();
     }
+  }
 
-    /**
-     * Draws galaxy state on canvas.
-     */
-    draw() {
-        noStroke();
+  /**
+   * Draws galaxy state on canvas.
+   */
+  draw() {
+    noStroke();
 
-        for (const particle of this.particles) {
-            particle.draw();
-        }
+    for (const particle of this.particles) {
+      particle.draw();
     }
+  }
 }
--- src/sketch.js
+++ src/sketch.js
@@ -2,18 +2,20 @@
  * Constants.
  */
 const BASE_SIZE = 600;
-const SPEED = 4;
+const INITIAL_SPEED = 2;
 
 /**
  * Sketch variables initialization.
  */
 let galaxy;
+let gauge;
 
 /**
  * Initializes sketch.
  */
 function setup() {
-  galaxy = new Galaxy(1000);
+  gauge = new Gauge(BASE_SIZE - 50, BASE_SIZE - 100, INITIAL_SPEED);
+  galaxy = new Galaxy(1000, gauge);
 
   var canvas = createCanvas(BASE_SIZE, BASE_SIZE);
   canvas.parent("sketch-holder");
@@ -24,8 +26,27 @@ function setup() {
  */
 function draw() {
   background(0);
-  translate(width / 2, height / 2);
 
+  push();
+  translate(width / 2, height / 2);
   galaxy.update();
   galaxy.draw();
+  pop();
+
+  gauge.draw();
+}
+
+/**
+ * Key pressed callback.
+ */
+function keyPressed() {
+  // pressed key was 'w'
+  if (keyCode === 87) {
+    gauge.accelerate();
+  }
+
+  // pressed key was 's'
+  else if (keyCode === 83) {
+    gauge.break();
+  }
 }
